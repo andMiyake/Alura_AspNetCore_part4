98638d8921e303a5b1d0d8b313ed95a4241cd9a5
Anderson Felipe Miyake <319013@ptin.corppt.com>
2021-12-06 22:22:33 -0300

    First commit

---
+++ antes/Item01/CasaDoCodigo.RelatorioWebAPI1/Controllers/RelatorioController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CasaDoCodigo.RelatorioWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelatorioController : ControllerBase
+    {
+        private static readonly List<string> Relatorio =
+            new List<string>()
+            {
+                "Primeiro pedido",
+                "Segundo pedido"
+            };
+
+        // GET: api/<RelatorioController>
+        [HttpGet]
+        public string Get()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in Relatorio)
+            {
+                stringBuilder.AppendLine(item);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        // POST api/Relatorio
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Relatorio.Add(value);
+        }
+
+    }
+}
---
+++ antes/Item05/CasaDoCodigo/Controllers/ContaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class ContaController : Controller
+    {
+        [Authorize]     // forçando a autorização do usuário
+        public async Task<ActionResult> Login()
+        {
+            // "~/" ->  calcula o endereço em tempo de execução para a home page
+            return Redirect(Url.Content("~/"));
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            //atualizar os cookies (autenticação local)
+            await HttpContext.SignOutAsync("Cookies");
+            //faz o desconexão no IdentityServer
+            await HttpContext.SignOutAsync("OpenIdConnect");
+        }
+    }
+}
